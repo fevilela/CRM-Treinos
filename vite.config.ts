8b3ee13b961b620fccf47518231e7d0626d3c8e0
fevilela <fernandavsac@gmail.com>
2025-09-22 07:41:32 -0300

    corrigindo tela inicial do aluno

--- vite.config.ts
+++ vite.config.ts
@@ -17,21 +17,21 @@ export default defineConfig({
         ]
       : []),
   ],
+  root: path.resolve(__dirname, "client"),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
+      "@": "/src",
       "@shared": path.resolve(__dirname, "shared"),
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: path.resolve(__dirname, "client"),
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true,
   },
   server: {
     host: "0.0.0.0",
-    port: 3000,
+    port: 5000,
     strictPort: true,
     allowedHosts: true,
     hmr: {
